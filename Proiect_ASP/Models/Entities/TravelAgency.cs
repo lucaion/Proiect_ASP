bbbd6f7e4fefda26be53fd46b8d9cb1ac2cd4b2e
Luca-Mihail Ion <luca.ion@s.unibuc.ro>
2022-09-04 12:37:17 +0300

    Done

--- Proiect_ASP/Models/Entities/TravelAgency.cs
+++ Proiect_ASP/Models/Entities/TravelAgency.cs
@@ -9,8 +9,8 @@ namespace Proiect_ASP.Models.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int? OwnerId { get; set; }
-        public Owner? Owner { get; set; }
+        public int OwnerId { get; set; }
+        public Owner Owner { get; set; }
         public int UserId { get; set; }
         public User User { get; set; }
         public ICollection<TravelAgencyOffer> TravelAgencyOffers { get; set; }
